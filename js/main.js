64567d92e71bc2197735de256d3e7852609b26d7
bogza.anton <bogza.anton@gmail.com>
2024-03-21 22:39:33 -0700

    Правки после первой проверки наставником

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import { UploadPhotoRenderer } from './upload-photo-renderer';
 import { ApiClient } from './api-client';
 import { MessagesRenderer } from './messages-renderer';
 import { PictureFilterRenderer } from './picture-filter-renderer';
+import { FeedPostsGenerator } from './feed-posts-generator';
 
 //Selectors
 const thumbnailsContainer = document.querySelector('.pictures');
@@ -12,18 +13,21 @@ const photoUploadInput = document.querySelector('.img-upload__input');
 //Classes Initialization
 const messageRenderer = new MessagesRenderer();
 const apiClient = new ApiClient('https://31.javascript.htmlacademy.pro/kekstagram/', messageRenderer);
+const feedPostGenerator = new FeedPostsGenerator();
 const picturesRenderer = new PicturesPreviewRenderer('picture');
 const pictureFullScreenRenderer = new PictureFullScreenRenderer('big-picture');
 const uploadPictureRenderer = new UploadPhotoRenderer('img-upload', apiClient, messageRenderer);
 
 //Logic
 apiClient.getData().then((feedPosts) => {
+  if (feedPosts === null) {
+    feedPosts = feedPostGenerator.generate();
+  }
+
   const filterRenderer = new PictureFilterRenderer('img-filters', feedPosts, thumbnailsContainer, picturesRenderer);
   filterRenderer.render();
-  if (feedPosts !== null) {
-    const thumbnailsFragments = picturesRenderer.render(feedPosts);
-    thumbnailsContainer?.appendChild(thumbnailsFragments);
-  }
+  const thumbnailsFragments = picturesRenderer.render(feedPosts);
+  thumbnailsContainer?.appendChild(thumbnailsFragments);
 
   //Subscriptions
   thumbnailsContainer.addEventListener('click', (evt) => {
